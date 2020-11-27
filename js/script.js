37a163814da0bc4a3ff3ba3da1e2c25a84666547
maxivaschenko <70215411+maxivaschenko@users.noreply.github.com>
2020-11-27 16:54:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-$(document).ready(function() {
-	$('.header__burger').click(function(event) {
-		$('.header__burger, .header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
+function	backToTop() {
+	let button = $('.back__to__top');
+
+	$(window).on('scroll', () => {
+		if ($(this).scrollTop() >= 200) {
+			button.fadeIn();
+		} else {
+			button.fadeOut();
+		}
+	});
+
+	button.on('click', (e) => {
+		e.preventDefault();
+		$('html').animate({scrollTop:0}, 1000);
 	})
-});
+}
+
+backToTop();
